e6a92a7b7fbe92933794255592137e026d4dc3b1
Nathan Mc <nathanmcneil@pm.me>
2020-05-26 17:05:20 -0700

    created routes for the express api

--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -1,19 +1,58 @@
 const router = require('express').Router()
-const List = require('../list.js')
+const fs = require('fs')
 
-const list = new List()
+router.get('/', (req, res) => {
+  res.sendFile(__dirname + '/../public/index.html')
+})
+
+router.get('/notes', (req, res) => {
+  res.sendFile(__dirname + '/../public/notes.html')
+})
 
 
 
 router.get('/api/notes', (req, res) => {
-  let items = list.getItems()
-  res.json(items)
+  let notes = fs.readFileSync(__dirname + '/../db/db.json')
+  notes = JSON.parse(notes)
+  res.json(notes)
 })
 
-router.post( )
+router.post('/api/notes', (req, res) => {
+  let notes = fs.readFileSync(__dirname + '/../db/db.json')
+  notes = JSON.parse(notes)
 
-router.put()
+  const note = {
+    id: notes.length,
+    title: req.body.title,
+    text: req.body.text,
+  }
 
-router.delete()
+  note.push(notes)
+
+  fs.writeFileSync(__dirname + '/../db/db.json', JSON.stringify(notes))
+
+  res.json(notes)
+})
+
+// router.put()
+
+router.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id
+  let notes = fs.readFileSync(__dirname + '/../db/db.json')
+  notes = JSON.parse(notes)
+
+  notes.filter(function(note) {
+    if (note.id === noteId) {
+      return false
+    }
+
+    return true
+  })
+
+  fs.writeFileSync(__dirname + '/../db/db.json', JSON.stringify(notes))
+
+  res.json(notes)
+
+})
 
 module.exports = router
